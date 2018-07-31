ea90d49cb891d903a45c552445a52ec63c550131
kwon <kkdosk@naver.com>
2018-07-31 19:28:37 +0900

    add

---
+++ bfs.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+from collections import defaultdict
+
+class Graph:
+  
+  def __init__(self):
+    self.graph = defaultdict(list)
+
+  def addEdge(self, u, v):
+      self.graph[u].append(v)
+
+  def BFS(self, s):
+    
