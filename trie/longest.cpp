37964459102770ecc58475c1d32d119d7bbb56fc
kwon <kkdosk@naver.com>
2018-08-08 19:30:48 +0900

    add

--- trie/longest.cpp
+++ trie/longest.cpp
@@ -1,5 +1,6 @@
 #include "../stdc++.h"
 #include <string>
+#include <stdbool.h>
 #define ARRAY_SIZE(a) sizeof(a)/sizeof(a[0])
 #define CHARACTERS 26
 #define CHAR_TO_INDEX(c) ((int)c - (int)'a')
@@ -12,7 +13,122 @@ struct TrieNode
   bool isLeaf;
 };
 
+struct TrieNode *getNode()
+{
+  struct TrieNode *myNode = new TrieNode;
+  myNode->isLeaf = false;
+
+  for(int i = 0; i < CHARACTERS; i++)
+  {
+    myNode->children[i] = NULL;
+  }
+  return myNode;
+}
+
+void Insert(struct TrieNode *root, string key)
+{
+  int len = key.length();
+  struct TrieNode *myNode = root, *temp = NULL;
+  char letter;
+  int index;
+
+  for(int i = 0; i < len; i++)
+  {
+    letter = key[i];
+    index = CHAR_TO_INDEX(letter);
+    if(myNode->children[index] == NULL)
+    {
+      myNode->children[index] = getNode();
+    }
+
+    myNode = myNode->children[index];
+  }
+  myNode->isLeaf = true;
+}
+
+bool Search(struct TrieNode *root, string key)
+{
+  int len = key.length();
+  int level = 0;
+  struct TrieNode *myNode = root;
+  char letter;
+  int index;
+
+  for(int i = 0; i < len; i++)
+  {
+    letter = key[i];
+    index = CHAR_TO_INDEX(letter);
+    if(myNode->children[index] == NULL)
+      return false;
+
+    myNode = myNode->children[index];
+  }
+
+  return (myNode && myNode->isLeaf);
+}
+
+bool is_final_node(struct TrieNode *root)
+{
+  if (root == NULL)
+    return false;
+  for (int i = 0; i < CHARACTERS; i++)
+    if (root->children[i] != NULL)
+      return false;
+  return true;
+}
+
+string LongestPrefixMatch(struct TrieNode *root, string word)
+{
+  int len = word.length();
+
+  string match, empty;
+  char letter;
+  int index;
+  bool found = false;
+  for(int i = 0; i < len && root; i++)
+  {
+    letter = word[i];
+    index = CHAR_TO_INDEX(letter);
+    if(root->children[index])
+      match.push_back(letter);
+    root = root->children[index];
+    if(root && root->isLeaf)
+      found = true;
+  }
+
+  if (found)
+    return match;
+  else
+    return empty;
+}
+
 int main() {
-  printf("%d\n", CHAR_TO_INDEX('z'));
+  string keys [] = {"the", "a", "there", "answer", "any", "caterer",
+                    "by", "bye", "their", "are", "area", "base", "basement", "len", "cater", 
+                    "cat", "catercr", "erer"};
+  string output[] = {"Not present in trie", "Present in trie"};
+
+  struct TrieNode *root = getNode();
+  int i;
+  for (i = 0; i < ARRAY_SIZE(keys); i++)
+  {
+    Insert(root, keys[i]);
+  }
+
+  string data[] = {"caterer", "child"};
+ 
+  for(int i=0; i< ARRAY_SIZE(data); i++)
+  {	
+    string ans;
+    string res = LongestPrefixMatch(root, data[i]);
+    if (res.length()==0) {
+      ans = output[0]; 
+      cout << data[i]<< " :: " << ans.c_str()<< endl;
+    }
+    else {
+      ans = res;
+      cout << data[i]<< " :: " << ans.c_str()<< endl;
+    }
+  }
   return 0;
 }
---
+++ trie/patternSearching.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<list>
+#define MAX_CHAR 256
+using namespace std;
+
+class SuffixTrieNode
+{
+private:
+  SuffixTrieNode *children[MAX_CHAR];
+  list<int> *indexes;
+public:
+  SuffixTrieNode()
+  {
+    indexes = new list<int>;
+
+    for(int i = 0; i < MAX_CHAR; i++)
+      children[i] = NULL;
+  }
+
+  void insertSuffix(string suffix, int index);
+
+  list<int>* search(string pat);
+};
+
+class SuffixTrie
+{
+private:
+  SuffixTrieNode root;
+public:
+  SuffixTrie(string txt)
+  {
+    for(int i = 0; i < txt.length(); i++)
+      root.insertSuffix(txt.substr(i), i);
+  }
+
+  void search(string pat);
+};
+
+void SuffixTrieNode::insertSuffix(string s, int index)
+{
+  indexes->push_back(index);
+
+  if(s.length() > 0)
+  {
+    char cIndex = s.at(0);
+
+    if(children[cIndex] == NULL)
+      children[cIndex] = new SuffixTrieNode();
+
+    children[cIndex]->insertSuffix(s.substr(1), index+1);
+  }
+}
+
+list<int>* SuffixTrieNode::search(string s)
+{
+  if(s.length() == 0)
+    return indexes;
+
+  if(children[s.at(0)] != NULL)
+    return (children[s.at(0)])->search(s.substr(1));
+
+  else return NULL;
+}
+
+void SuffixTrie::search(string pat)
+{
+  list<int> *result = root.search(pat);
+
+  if(result == NULL)
+    cout << "Pattern not found" << endl;
+  else
+  {
+    list<int>::iterator i;
+    int patLen = pat.length();
+    for(i = result->begin(); i != result->end(); ++i) {
+      cout << "Pattern found at position " << *i - patLen << endl;
+    }
+  }
+}
+
+int main() {
+  string txt = "geeksforgeeks.org";
+  SuffixTrie S(txt);
+
+  cout << "Search for 'ee'" << endl;
+  S.search("ee");
+
+  cout << "\nSearch for 'geek'" << endl;
+  S.search("geek");
+
+  cout << "\nSearch for 'quiz'" << endl;
+  S.search("quiz");
+
+  cout << "\nSearch for 'forgeeks'" << endl;
+  S.search("forgeeks");
+  
+  return 0;
+}
